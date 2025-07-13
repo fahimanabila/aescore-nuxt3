9530fb29263e99ee4184c0e8b26671c73154d0d4
fahimanabila <fahimanabila82@gmail.com>
2025-07-14 02:21:25 +0700

    update : fix again

--- pages/grade/index.vue
+++ pages/grade/index.vue
@@ -600,7 +600,7 @@ Respond strictly in the following **JSON** format. DO NOT! include any extra exp
     max_token: 300
   }
   try {
-    const apiResponse = await fetch(`${apiBaseUrl}/api/generate`, {
+    const apiResponse = await fetch(`https://e0f448fd914c.ngrok-free.app/api/generate`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
